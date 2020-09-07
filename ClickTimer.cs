83f9b4192ee0f3d3b75a81a7118a6a52791e07e2
MinShiGee <46314169+MinShiGee@users.noreply.github.com>
2020-09-07 12:49:28 +0900

    Add files via upload

---
+++ ClickTimer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class ClickTimer : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Sample = default;
+    [SerializeField] private float clickTime = 0f;
+    [SerializeField] private bool isClick = false;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        Timer();
+    }
+    void Timer()
+    {
+        text.text = clickTime.ToString();
+        if (Input.GetMouseButtonUp(0))
+        {
+            //Shoot(clickTime);
+            clickTimeInit();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            isClick = true;
+        }
+        if (isClick)
+            clickTime += Time.deltaTime;
+
+    }
+    void clickTimeInit()
+    {
+        clickTime = 0;
+        isClick = false;
+    }
+    /*void Shoot(float power)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (col.Raycast(ray, out hit, 600) && Input.GetMouseButtonUp(0))
+        {
+            GameObject sample = Instantiate(Sample, hit.point, Sample.transform.rotation);
+            SampleTest st = sample.GetComponent<SampleTest>();
+            st.Shoot(power);
+        }
+    }*/
+    
+}
