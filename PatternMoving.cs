34006e65540b1c511b8b2d0a11c238409044377c
MinShiGee <46314169+MinShiGee@users.noreply.github.com>
2020-09-07 12:52:45 +0900

    Add files via upload

---
+++ PatternMoving.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PatternMoving : MonoBehaviour
+{
+    [SerializeField] [Range(0, 3)] private float range = 0f;
+    [SerializeField] [Range(0, 3)] private float speed = 1f;
+    private Vector3 pos = default;
+    private enum Direction {right,left};
+    Direction dir = Direction.right;
+
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Check();
+        switch (dir)
+        {
+            case (Direction.left):
+                MoveLeft();
+                break;
+            case (Direction.right):
+                MoveRight();
+                break;
+            default:
+                break;
+        }
+    }
+    void Check()
+    {
+        if(transform.position.x <= pos.x - range)
+        {
+            dir = Direction.right;
+        }
+        if (transform.position.x >= pos.x + range)
+        {
+            dir = Direction.left;
+        }
+    }
+    void MoveLeft()
+    {
+        transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+    }
+    void MoveRight()
+    {
+        transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+    }
+}
