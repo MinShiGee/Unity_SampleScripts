750ad482c81878253480d0019e7ec3c3395a8115
MinShiGee <46314169+MinShiGee@users.noreply.github.com>
2020-10-13 07:46:45 +0900

    Add files via upload

---
+++ FPSEnemyAI/Attackable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Attackable : MonoBehaviour
+{
+    [SerializeField] protected float maxHealth = 100f;
+    [SerializeField] protected float health = 100f;
+    [SerializeField] protected float maxStamina = 100f;
+    [SerializeField] protected float stamina = 100f;
+
+    [SerializeField] protected bool isDeath = false;
+
+    public float GetHealth() {return health;}
+    public void SetHealth(float hp){ health = (hp > maxHealth) ? maxHealth : hp;}
+    public void AddHealth(float hp){
+        health += hp;
+        if(health > maxHealth)
+            health = maxHealth;
+        if(health <= 0){
+            Death();
+        }
+    }
+    protected virtual void Death(){
+        isDeath = true;
+    }
+    public float GetMaxHealth(){return maxHealth;}
+    public void SetMaxHealth(float mHP){maxHealth = mHP;}
+
+    public void GetDamage(float dam){
+        AddHealth(-dam);
+    }
+
+    public float GetStamina(){return stamina;}
+    public void SetStamina(float st){ stamina = (st > maxStamina) ? maxStamina : st;}
+    public void AddStamina(float st){
+        stamina += st;
+        if(stamina > maxStamina)
+            stamina = maxStamina;
+        if(stamina <= 0){
+            stamina = 0;
+        }
+    }
+    public float GetMaxStamina(){return maxStamina;}
+    public void SetMaxStamina(float mST){maxStamina = mST;}
+
+
+}
---
+++ FPSEnemyAI/HumanAI.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class HumanAI : Attackable
+{
+#region AI Init Data
+
+    [Header("AI Data")]
+    [SerializeField] protected string human_Name = "";
+
+    [Header("AI difficulty")]
+    [Range(0, 10)] [SerializeField] private int shooting_Skills = 5;
+    [Range(0, 10)] [SerializeField] private int walking_Speed = 3;
+    [Range(0, 10)] [SerializeField] private int runnning_Speed = 7;
+
+    [Header("Checking")]
+    [SerializeField] protected Vector3 chasingPos = default;
+    [SerializeField] protected GameObject chasingTarget = null;
+
+    protected enum State{
+        Idle, // Resting
+        Searching, // Finding Player
+        ChasePos,
+        ChaseTarget
+    }
+    [SerializeField] protected State state;
+
+    [Header("Eye Data")]
+    [SerializeField] private float eye_angle = 60f;
+    [SerializeField] private float eye_distance = 50f;
+    [SerializeField] private LayerMask ob_layerMask = 1 << 10;
+    [SerializeField] private GameObject inSightObject = null;
+
+    protected NavMeshAgent navMesh;
+    protected Rigidbody rigidbody;
+
+#endregion
+
+    protected virtual void Start(){
+        AI_Init();
+        StartCoroutine("DetectFunc");
+    }
+
+    protected virtual void Update(){
+        switch(state){
+            case State.Idle:
+            Idle();
+            break;
+            case State.Searching:
+            Searching();
+            break;
+            case State.ChasePos:
+            ChasePos();
+            break;
+            case State.ChaseTarget:
+            ChaseTarget();
+            break;
+            default:
+            break;
+        }
+    }
+
+#region DetectFunc
+
+    private IEnumerator DetectFunc(){
+        var wait = new WaitForSeconds(0.15f);
+        while(true){
+            SetTarget();
+            yield return wait;
+        }
+    }
+    private void SetTarget(){
+        EyeFindTarget();
+    }
+    private void EyeFindTarget(){
+
+        Collider[] ob_cols = Physics.OverlapSphere(transform.position, eye_distance, ob_layerMask);
+
+        if(ob_cols.Length < 1)
+            return;
+
+        Transform tar_trans = ob_cols[0].transform;
+
+        Vector3 eye_direction = (tar_trans.position - transform.position).normalized;
+        float angle = Vector3.Angle(eye_direction, transform.forward);
+
+        if(angle < eye_angle * 0.5f){
+
+            if(Physics.Raycast(transform.position, eye_direction, out RaycastHit hit, eye_distance)){
+                if(hit.transform.gameObject.layer == 10){
+                    inSightObject = hit.transform.gameObject;
+                    return;
+                }
+            }
+
+        }
+        inSightObject = null;
+    }
+
+
+#endregion
+
+#region  AI_FSM
+    protected virtual void Idle(){
+
+    }
+    protected virtual void Searching(){
+        
+    }
+    protected virtual void ChasePos(){
+        
+    }
+    protected virtual void ChaseTarget(){
+        
+    }
+#endregion
+
+#region Initializing
+    protected virtual void AI_Init(){
+        navMesh = this.GetComponent<NavMeshAgent>();
+        rigidbody = this.GetComponent<Rigidbody>();
+        this.gameObject.layer = 9;
+        SetTag();
+        SetState(State.Idle);
+        InitTarget();
+
+    }
+    protected virtual void SetTag(){
+        this.gameObject.tag = "Human";
+    }
+    protected virtual void SetState(State data){
+        this.state = data;
+    }
+    protected void InitTarget(){
+        chasingPos = default;
+        chasingTarget = null;
+    }
+
+#endregion
+
+
+}
